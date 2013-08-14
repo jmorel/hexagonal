2af827feda0fb8b65517e48533ad4e05baa3a03d
Jérémy Morel <morel.jeremy@gmail.com>
2013-08-14 15:13:58 +0200

    New slot structure (OO)

--- position.js
+++ position.js
@@ -1,100 +1,92 @@
-// PARAMETERS
+// useful aliases
+var PI = Math.PI;
+var cos = Math.cos;
+var sin = Math.sin;
 
-/*var THETA = Math.PI / 20;
-var COS = Math.cos(THETA);
-var SIN = Math.sin(THETA);
-var UNIT = 160;
-var GRID_UNIT = 160;
-
-var elts = document.querySelectorAll('.positioned');
-for(var i=0; i<elts.length; i++) {
-	var e = elts[i];
-	var row = parseInt(e.getAttribute('data-row'));
-	var col = parseInt(e.getAttribute('data-col'));
-	var shiftLeft = GRID_UNIT * (col * COS + row * SIN);
-	var shiftTop = GRID_UNIT * (row * COS - col * SIN);
-	e.style.left = parseInt(shiftLeft).toString() + 'px';
-	e.style.top = parseInt(shiftTop).toString() + 'px';
-	console.log("TOP: "+shiftTop+" / LEFT: "+shiftLeft);
-}*/
-
-/*var THETA = Math.PI / 20;
-var COS = Math.cos(THETA);
-var SIN = Math.sin(THETA);
-var RADIUS = 80;
-
-var elts = document.querySelectorAll('.positioned');
-for(var i=0; i<elts.length; i++) {
-	var e = elts[i];
-	var row = parseInt(e.getAttribute('data-row'));
-	var col = parseInt(e.getAttribute('data-col'));
-	
-	var centerLeft = RADIUS * (col - row / 2);
-	var centerTop = RADIUS * row;
-
-	//var shiftLeft = centerLeft - RADIUS;
-	//var shiftTop = centerTop - RADIUS*Math.sqrt(3)/2;
-
-	var shiftLeft = centerLeft;
-	var shiftTop = centerTop;
-	
-
-	e.style.left = parseInt(shiftLeft).toString() + 'px';
-	e.style.top = parseInt(shiftTop).toString() + 'px';
-	
-	console.log("TOP: "+shiftTop+" / LEFT: "+shiftLeft);
-}*/
-
-var size = 'hexagon-bg-1.png',
-	slots = [],
-	slotShadow = new Image();
-
-slotShadow.className = "positioned";
-slotShadow.style.opacity = 0.4;
-slotShadow.src = 'hexagon-bg-1.png';
-document.body.appendChild(slotShadow);
-
-function Slot(x, y) {
-	var slot = new Image();
-	slot.src = size;
-	slot.className = "positioned";
-	slot.onload = function() {
-		console.log(x+" - "+y);
-		x -= this.width/2;
-		y -= this.height/2;
-		this.style.top = y+"px";
-		this.style.left = x+"px";
-		console.log(x+" - "+y);
-	}
-	return slot;
+// basic directions within a slot
+var shiftTheta = PI / 20,
+	baseTheta = PI / 3,
+	directions = [];
+for ( var i=0; i<6; i++ ) {
+	directions.push( [ cos( shiftTheta+i*baseTheta ), sin( shiftTheta+i*baseTheta ) ] );
 }
 
-function newSlot(evt) {
+// templates
+var slotTemplates = [ 
+		{ src: 'hexagon-bg-1.png', r: 160, h: 160*cos(PI/3)},
+		{ src: 'hexagon-bg-2.png', r: 320, h: 320*cos(PI/3)},
+		{ src: 'hexagon-bg-3.png', r: 480, h: 480*cos(PI/3)} ] ,
+	templateID = 0;
+
+// slots
+function Slot(x, y) {
+	var self = this;
+	// basic parameters: center position and height
+	this.x = x;
+	this.y = y;
+	this.h = slotTemplates[templateID].h;
+
+	// attach image
+	this.img = new Image();
+	this.img.src = slotTemplates[templateID].src;
+	this.img.onload = function() { self.updatePosition(); };
+
+	// load into the document
+	document.body.appendChild(this.img);
+
+	this.updatePosition = function(x, y) {
+		console.log("updatePosition: "+x+" - "+y+" / "+this);
+		if ( x && y ) {
+			this.x = x;
+			this.y = y;
+		}
+		this.img.style.top = ( this.y - this.img.height/2 ) + "px";
+		this.img.style.left = ( this.x - this.img.width/2 ) + "px";
+	}
+
+	this.setOpacity = function(opacity) {
+		this.img.style.opacity = opacity;
+	}
+
+	this.updateTemplate = function() {
+		this.h = slotTemplates[templateID].h;
+		this.img.src = slotTemplates[templateID].src;
+		this.updatePosition();
+	}
+}
+
+var slots = [],
+	cursorShadow = new Slot();
+
+// setup cursorShadow
+cursorShadow.setOpacity( 0.4 );
+
+
+
+
+function addSlot(evt) {
 	//if(!slots.length) {
 		var slot = new Slot(evt.clientX, evt.clientY);
 		slots.push(slot);
-		document.body.appendChild(slot);
+		document.body.appendChild(slot.img);
 	//}
 }
 
-function changeSize(evt) {
+function changeTemplate(evt) {
 	if ( evt.charCode == 49 ) { 
-		size = 'hexagon-bg-1.png';
-		slotShadow.src = 'hexagon-bg-1.png';
+		templateID = 0;
 	} else if ( evt.charCode == 50 ) {
-		size = 'hexagon-bg-2.png';
-		slotShadow.src = 'hexagon-bg-2.png';
+		templateID = 1;
 	} else if ( evt.charCode == 51 ) { 
-		size = 'hexagon-bg-3.png'; 
-		slotShadow.src = 'hexagon-bg-3.png';
+		templateID = 2;
 	}
+	cursorShadow.updateTemplate();
 }
 
-function dropSlotShadow(evt) {
-	slotShadow.style.left = ( evt.clientX - slotShadow.width/2 ) + "px";
-	slotShadow.style.top = ( evt.clientY - slotShadow.height/2 ) + "px";
+function dropCursorShadow(evt) {
+	cursorShadow.updatePosition( evt.clientX, evt.clientY );
 }
 
-document.onmousemove = dropSlotShadow;
-document.onclick = newSlot;
-document.onkeypress = changeSize;
+document.onmousemove = dropCursorShadow;
+document.onclick = addSlot;
+document.onkeypress = changeTemplate;
